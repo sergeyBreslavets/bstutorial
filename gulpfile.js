8df1415126faac4b2a000840811fc88daad9a4f8
sergeyBreslavets <venom.bren@gmail.com>
2015-09-18 00:26:48 +0400

    add start file. config file.  update path in gulp

---
+++ gulpfile.js
@@ -0,0 +1,160 @@
+'use strict';
+
+// include gulp
+var gulp       = require('gulp');
+
+// include plug-ins
+var changed        = require('gulp-changed');
+var imagemin       = require('gulp-imagemin');
+var minifyHTML     = require('gulp-minify-html');
+var jshint         = require('gulp-jshint');
+var concat         = require('gulp-concat');
+var stripDebug     = require('gulp-strip-debug');
+var uglify         = require('gulp-uglify');
+var autoprefix     = require('gulp-autoprefixer');
+var minifyCSS      = require('gulp-minify-css');
+var sass           = require('gulp-sass');
+var jade           = require('gulp-jade');
+
+//src file
+var imgSrc         = './src/images/**/*';
+var sourcesjs      =  [     './src/scripts/*.js', 
+                            'bower_components/modernizr/modernizr.js',
+                            'bower_components/jquery/dist/jquery.js',
+                            'bower_components/bootstrap/js/transition.js',
+                            'bower_components/bootstrap/js/alert.js',
+                            'bower_components/bootstrap/js/button.js',
+                            'bower_components/bootstrap/js/carousel.js',
+                            'bower_components/bootstrap/js/collapse.js',
+                            'bower_components/bootstrap/js/dropdown.js',
+                            'bower_components/bootstrap/js/modal.js',
+                            'bower_components/bootstrap/js/tooltip.js',
+                            'bower_components/bootstrap/js/popover.js',
+                            'bower_components/bootstrap/js/scrollspy.js',
+                            'bower_components/bootstrap/js/tab.js',
+                            'bower_components/bootstrap/js/affix.js',
+                            'bower_components/jquery/dist/jquery.js'
+
+
+                            //'bower_components/OwlCarousel/owl-carousel/owl.carousel.js',
+                            //'bower_components/magnific-popup/dist/jquery.magnific-popup.js',
+                           // 'bower_components/shufflejs/dist/jquery.shufflejs.js',
+                           // 'bower_components/wowjs/dist/wow.js',
+                            //'bower_components/isotope/dist/isotope.pkgd.js',
+                            //'bower_components/imagesloaded/imagesloaded.js',
+                           // 'bower_components/jquery.ajax-progress.js',
+                           // 'bower_components/jquery.fitvids.js',
+                           // 'bower_components/jquery.parallax-1.1.3.js',
+                           // 'bower_components/jquery.sticky.js',
+                           //Ы 'bower_components/_contact.js'
+                            //'src/js/main-init.js'
+                            ];
+
+var htmlSrc        = './src/html/*.html';
+var srcjade        ='./src/jade/*.jade';
+var srcsass        ='./src/sass/styles.scss';
+
+//src target
+var csstarget      = './www/assets/styles/';
+var htmlDst        = './www/';
+var sasstarget     = './www/assets/styles';
+var pathjstarget   = './www/assets/scripts/';
+var fontsTarget    = './www/assets/fonts/';
+var imgDst         = './www/assets/images';
+var jadetarget     = './www/';
+
+// tasks 
+gulp.task('copyfont', function() {
+    gulp.src('./bower_components/material-design-icons/iconfont/*')
+        .pipe(gulp.dest(fontsTarget));
+});
+
+//jade task
+gulp.task('jade', function() {
+  gulp.src([srcjade])
+    .pipe(jade())
+    .pipe(gulp.dest(jadetarget))
+
+}); 
+
+// JS hint task
+gulp.task('jshint', function() {
+    gulp.src('./src/scripts/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+// minify new images
+gulp.task('imagemin', function() {
+    gulp.src(imgSrc)
+        .pipe(changed(imgDst))
+        .pipe(imagemin())
+        .pipe(gulp.dest(imgDst));
+});
+// minify new or changed HTML pages
+gulp.task('htmlpage', function() {
+    gulp.src(htmlSrc)
+        .pipe(changed(htmlDst))
+        .pipe(minifyHTML())
+        .pipe(gulp.dest(htmlDst));
+});
+// JS concat, strip debugging and minify
+gulp.task('scripts', function() {
+   gulp.src(sourcesjs)
+        .pipe(concat('script.js'))
+        .pipe(stripDebug())
+        .pipe(uglify())
+
+    .pipe(gulp.dest(pathjstarget));
+});
+
+// CSS concat, auto-prefix and minify
+gulp.task('styles', function() {
+    gulp.src(['./src/styles/*.css'])
+        .pipe(concat('styles.css'))
+        .pipe(autoprefix('last 2 versions'))
+        .pipe(minifyCSS())
+        .pipe(gulp.dest(csstarget));
+});
+
+//sass task
+
+gulp.task('sass', function() {
+    gulp.src(srcsass)
+        /*.pipe(sourcemaps.init())*/
+        .pipe(sass({
+            errLogToConsole: true
+        }))
+        // .pipe(sourcemaps.write())
+        .pipe(gulp.dest(sasstarget));
+});
+
+// default gulp task
+gulp.task('default', ['imagemin', 'htmlpage',  'scripts', 'styles', 'sass', /*'jade'*/], function() {
+    // watch for HTML changes
+   gulp.watch('./src/*.html', function() {
+    gulp.run('htmlpage');
+    });
+
+    // watch for JS changes
+    gulp.watch('./src/scripts/*.js', function() {
+        gulp.run('jshint', 'scripts');
+    });
+
+    // watch for CSS changes
+    gulp.watch('./src/styles/*.css', function() {
+        gulp.run('styles');
+    });
+    gulp.watch('./src/sass/{,*/}*.{scss,sass}', function() {
+        gulp.run('sass');
+    });
+    gulp.watch('./src/images/**/*', function() {
+        gulp.run('imagemin');
+    });
+
+  // gulp.task('./src/jade/**/*', function(){
+  // gulp.run('jade');
+   //});
+
+});
+
+
